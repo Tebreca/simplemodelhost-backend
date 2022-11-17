777a8a0981ff06bea9f53f00c37e1042ee2a1bc8
Ewan Arends <ewan.arends10@gmail.com>
2022-11-17 15:35:49 +0100

    Spring initializr

---
+++ src/main/java/com/tebreca/simplemodelhost/SimplemodelhostApplication.java
@@ -0,0 +1,13 @@
+package com.tebreca.simplemodelhost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplemodelhostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplemodelhostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tebreca/simplemodelhost/SimplemodelhostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tebreca.simplemodelhost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplemodelhostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
